ce4c73de2703f98e7abee7f21d17e44d5f1c1c16
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-08-11 16:52:42 +0700

    Initial commit, empty implementation

---
+++ lib/Hash/DefHash.pm
@@ -0,0 +1,22 @@
+package Hash::DefHash;
+
+use 5.010;
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+# ABSTRACT: Manipulate defhashes
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<DefHash> specification
+
+=cut
